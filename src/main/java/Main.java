dbdf6b86420777712e44a2cb8a37cddb0ca34c3a
pz307hn <peteyorn>
2017-06-19 12:29:47 +0200

    refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,27 +8,9 @@ import java.util.*;
  * @author Peter Zemianek
  */
 public class Main {
-    
+
     public static void main(String[] args) {
 
-        String[] defaultArgs = new String[]
-                {
-                        "-gui", "false",
-                        "-welcome", "false",
-                        "-cli", "false",
-                        "-printpass", "false"
-                };
-
-        Map<String, String> agents = new HashMap<String, String>();
-       /* agents.put("Reciever", "sk.tuke.fei.bdi.emotionalengine.parser.parser_example.RecieverBDI.class");
-        agents.put("Sender", "sk.tuke.fei.bdi.emotionalengine.parser.parser_example.SenderBDI.class");*/
-
-       agents.put("TelemarketerAnna", "sk.tuke.fei.bdi.emotionalengine.example.telemarketeranna.TelemarketerAnnaBDI.class");
-       agents.put("HungryPaul", "sk.tuke.fei.bdi.emotionalengine.example.hungrypaul.HungryPaulBDI.class");
-
-        JBDIEmo.start(agents);
-
-//        System.err.println("\n" + CryGoal.class.getAnnotation(EmotionalGoal.class).value().length + " :( FAKT AKO TA JA NEVJEM UZ");
 
     }
 }
--- src/main/java/sk/tuke/fei/bdi/emotionalengine/example/ExampleStarter.java
+++ src/main/java/sk/tuke/fei/bdi/emotionalengine/example/ExampleStarter.java
@@ -1,7 +1,22 @@
 package sk.tuke.fei.bdi.emotionalengine.example;
 
+import sk.tuke.fei.bdi.emotionalengine.starter.JBDIEmo;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by PeterZemianek on 5/14/2017.
  */
 public class ExampleStarter {
+
+    public static void main(String[] args) {
+
+        Map<String, String> agents = new HashMap<>();
+
+        agents.put("TelemarketerAnna", "sk.tuke.fei.bdi.emotionalengine.example.telemarketeranna.TelemarketerAnnaBDI.class");
+        agents.put("HungryPaul", "sk.tuke.fei.bdi.emotionalengine.example.hungrypaul.HungryPaulBDI.class");
+
+        JBDIEmo.start(agents);
+    }
 }
